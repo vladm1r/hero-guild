32b763ea9e61ca07040c18e45a6dc5a825714669
vladm1r <v13911@mail.ru>
2022-04-22 22:10:04 +0300

    added nav

---
+++ src/components/NavItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  link: String,
+});
+</script>
+
+<template>
+  <a
+    :href="props.link"
+    class="w-20 h-20 rounded-full bg-yellow-600 hover:bg-amber-700 flex items-center justify-center border-4 hover:scale-105 border-amber-700 transition-all"
+  >
+    <slot />
+  </a>
+</template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import Nav from "./TheNav.vue";
+</script>
 
 <template>
   <div
-    class="h-24 bg-amber-300 rounded-b-lg border-x-2 border-b-2 border-amber-700 shadow-md"
-  ></div>
+    class="h-24 bg-amber-300 rounded-b-lg border-x-2 border-b-2 border-amber-700 shadow-md flex items-center justify-center"
+  >
+    <Nav />
+  </div>
 </template>
